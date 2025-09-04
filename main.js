197389f1846ea374a7b11bae5c03487e4bbf2557
kaiogabr <kaio.cadina.pereira@escola.pr.gov.br>
2025-09-04 09:00:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,15 @@
 var pacientes = document.querySelector("subtitulo");
 var nome = document.querySelector("titulo");
 nome.textContent = "Kaio Nutrição";
-pacientes.textContent = "Meus pacientes";
+titulo.textContent = "Meus pacientes";
+subtitulo.content = "Meus Pacientes"
 
 //acessar a tag tr - paciente Paulo
-var paciente = document.querySelector("#primeiro paciente");
+var paciente = document.querySelectorAll("#primeiro paciente");
+for(var 1 = 0; 1 < paciente.length; 1++){
+    1 = 
+}
+
 
 //seleciona o conteúdo peso da tag
 var tdpeso = paciente.querySelector(".info-peso");
@@ -18,15 +23,27 @@ var altura = tdaltura.textContent;
 //calcula o imc
 var imc = peso / (altura * altura);
 
+//variaveis com valor true
+var pesovalido = true;
+var alturavalido = true;
+tdImc.textContent = "Altura valida"
+
+
 //acessa e altera o imc
 var tdimc = paciente.querySelector(".info-imc");
 tdimc.textContent = imc;
 
+
 if(peso <0 || peso > 1000){
     alert("peso invalido")
+
+var pesovalido = false;
+
 }
 if(altura < 0 || altura> 3.00){
     alert("altura invalida");
+
+var alturavalida = false;
  
 }
 
