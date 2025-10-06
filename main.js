1b97339c3a2f2c917d4a4c4d061e14d98465898c
kaiogabr <kaio.cadina.pereira@escola.pr.gov.br>
2025-10-06 11:01:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -78,7 +78,6 @@ subtitulo.content = "Meus Pacientes";
 
  //acessa o formulario
  var formulario = document.querySelector("#form-adiciona");
- console.log(formulario);
 
  //caça os valores
  var nome = formulario.nome.value;
@@ -88,6 +87,7 @@ subtitulo.content = "Meus Pacientes";
 
  //cria tag tr
  var pacienteTr = document.createElement("tr");
+
  var nomeTd = document.createElement("td");
  var pesoTd = document.createElement("td");
  var alturaTd = document.createElement("td");
@@ -101,8 +101,18 @@ subtitulo.content = "Meus Pacientes";
  gorduraTd.textcontent = gordura;
  imcTd.textcontent = imc;
 
+ //Adiciona as tags na tela do usuario
+      pacienteTr.appendChild(nomeTd);
+      pacienteTr.appendChild(pesoTd);
+      pacienteTr.appendChild(alturaTd);
+      pacienteTr.appendChild(gorduraTd);
+      pacienteTr.appendChild(imcTd);
+
+      var tabela = document.querySelector("#tabela-pacientes");
+      tabela.appendChild(pacienteTr);
 
     });
 
-   
 
+   
+            
